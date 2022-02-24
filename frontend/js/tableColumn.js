5d518a216ff36026eab87fc769c4154a41954c3e
Hasham M Khan <hasham.m.khan@outlook.com>
2022-02-24 17:47:51 -0600

    updated adding columns

--- frontend/js/tableColumn.js
+++ frontend/js/tableColumn.js
@@ -1,7 +1,35 @@
-const selectTypeElements = document.querySelectorAll('.select-type')
-selectTypeElements.forEach((element) => {
-    element.addEventListener('click', selectSpan)
-})
+let showCreateButton = false;
+let rosterColCount = 0;
+
+
+// ADD COLUMN EVENT
+const addButton = document.querySelector('#addButton')
+addButton.addEventListener('click', createRosterColumn)
+
+function createRosterColumn () {
+    let rosterColumnTemplate = document.querySelector('#tableColumn')
+
+    let columnHTML = document.importNode(rosterColumnTemplate.content, true)
+
+    // Adding IDs to the fieldset and the delete icon
+    rosterColCount++
+    columnHTML.children[0].id = `rsCol${rosterColCount}`
+    let deleteColElement = columnHTML.children[0].querySelector('.del-ico')
+    deleteColElement.id = `_${rosterColCount}`
+    deleteColElement.addEventListener('click', deleteColumn)
+    
+    let rosterColumnsElement = document.querySelector('#rosterColumns')
+    rosterColumnsElement.appendChild(columnHTML)
+
+    addListenerToOptions()
+}
+
+function addListenerToOptions () {
+    const selectTypeElements = document.querySelectorAll('.select-type')
+    selectTypeElements.forEach((element) => {
+        element.addEventListener('click', selectSpan)
+    })
+}
 
 function selectSpan (e) {
     if (!e.target.classList.contains("select-type")) {
@@ -16,4 +44,12 @@ function selectSpan (e) {
         }
     }
 
+}
+
+// REMOVING COLUMNS
+function deleteColumn (e) {
+    let idNumber = e.target.id.slice(1, e.target.id.length)
+    
+    let colElement = document.querySelector(`#rsCol${idNumber}`)
+    colElement.remove()
 }
