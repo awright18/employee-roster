8e0fadd6f761b22bd5cece752849ab609f91166c
Hasham M Khan <hasham.m.khan@outlook.com>
2022-02-26 05:32:12 -0600

    updated adding columns for roster settings

--- frontend/js/tableColumn.js
+++ frontend/js/tableColumn.js
@@ -1,49 +1,20 @@
-let showCreateButton = false;
-let rosterColCount = 0;
+import { Column, ColumnOption } from'./toggleOptions.js'
 
+let showCreateButton = false
+let rosterColCount = 0
+let columnsList = []
 
 // ADD COLUMN EVENT
 const addButton = document.querySelector('#addButton')
 addButton.addEventListener('click', createRosterColumn)
 
 function createRosterColumn () {
-    let rosterColumnTemplate = document.querySelector('#tableColumn')
+    const columnsElement = document.querySelector('#rosterColumns')
 
-    let columnHTML = document.importNode(rosterColumnTemplate.content, true)
+    let column = new Column()
+    columnsList.push(column.columnContent)
 
-    // Adding IDs to the fieldset and the delete icon
-    rosterColCount++
-    columnHTML.children[0].id = `rsCol${rosterColCount}`
-    let deleteColElement = columnHTML.children[0].querySelector('.del-ico')
-    deleteColElement.id = `_${rosterColCount}`
-    deleteColElement.addEventListener('click', deleteColumn)
-    
-    let rosterColumnsElement = document.querySelector('#rosterColumns')
-    rosterColumnsElement.appendChild(columnHTML)
-
-    addListenerToOptions()
-}
-
-function addListenerToOptions () {
-    const selectTypeElements = document.querySelectorAll('.select-type')
-    selectTypeElements.forEach((element) => {
-        element.addEventListener('click', selectSpan)
-    })
-}
-
-function selectSpan (e) {
-    if (!e.target.classList.contains("select-type")) {
-        const parentElement = e.target.parentElement
-
-        for (let child of parentElement.children) {
-            if (child === e.target) {
-                child.classList.toggle('bg-info')
-            } else {
-                child.classList.remove('bg-info')
-            }
-        }
-    }
-
+    columnsElement.appendChild(column.columnContent)
 }
 
 // REMOVING COLUMNS
---
+++ frontend/js/toggleOptions.js
@@ -0,0 +1,76 @@
+export class Column {
+    #options;
+
+    constructor () {
+        this.#options = []
+
+        let templateHTML = document.getElementById('tableColumn').content
+        this.columnContent = templateHTML.cloneNode(true)
+
+        this.createOptions()
+        this.addCloseButton()
+    }
+
+    createOptions () {
+        let optionElements = this.columnContent.querySelectorAll('.column-option')
+        optionElements.forEach((option) => {
+            let optionObj = new ColumnOption(option, !option.classList.contains('not-empty'))
+            this.addOption(optionObj)
+        })
+    }
+
+    addCloseButton () {
+        let closeButton = this.columnContent.querySelector('.del-ico')
+        closeButton.addEventListener('click', this.deleteColumn())
+    }
+
+    getOptions () {
+        return this.#options
+    }
+    
+    addOption (option) {
+        this.#options.push(option)
+    }
+
+    deleteColumn () {
+        let columnsElement = document.querySelector('#rosterColumns')
+        let baseElement = this.columnContent.querySelector('fieldset')
+        // columnsElement.remove(baseElement)
+
+        console.log(baseElement)
+    }
+}
+
+
+export class ColumnOption {
+
+    constructor (parentElement, allowEmpty) {
+        this.parentElement = parentElement
+        this.allowEmpty = allowEmpty
+        this.children = []
+        this.isAnOptionSelected = false
+        this.selectedElement
+
+        for (let childElement of this.parentElement.children) {
+            this.children.push({
+                element: childElement,
+                allowDeselect: allowEmpty,
+                isSelected: false
+            })
+        }
+
+        this.parentElement.addEventListener('click', this.toggleOptions)
+
+        // this.children = [...this.element.chldren].map
+    }
+
+    toggleOptions (click) {
+        
+        // ONLY proceed if the clicked element is not the parent
+        if (click.target != this.parentElement) {
+            
+
+
+        }
+    }
+}
